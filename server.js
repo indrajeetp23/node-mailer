0637fe6bd332269082687fa87fe93ab881d546db
Indrajeet patel <indrajeetpatel2308@gmail.com>
2026-01-30 12:14:54 +0530

    Refactor server port handling

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post("/send-mail", async (req, res) => {
     });
   }
 });
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
